a345344c45673358f9db233274506746152a937f
svdu <sdurnev@gmail.com>
2019-12-25 01:31:17 +0600

    paramBCM is redy

--- main.go
+++ main.go
@@ -6,12 +6,42 @@ package main
 
 import (
 	"fmt"
-	"log"
-
 	g "github.com/soniah/gosnmp"
+	"log"
+	"math/big"
 )
 
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!! VERSION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+const version = "0.00.1"
+
+/*
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!! VERSION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+*/
+
 func main() {
+
+	paramBCMmib := []string{"1.3.6.1.4.1.26896.1.3.1",
+		"1.3.6.1.4.1.26896.1.3.2",
+		"1.3.6.1.4.1.26896.1.3.3",
+		"1.3.6.1.4.1.26896.1.3.4",
+		"1.3.6.1.4.1.26896.1.3.5",
+		"1.3.6.1.4.1.26896.1.3.6",
+		"1.3.6.1.4.1.26896.1.3.7"}
+
+	paramBCM := [...]string{"bcmSystemVoltage",
+		"bcmLoadCurrent",
+		"bcmBatteryCurrent",
+		"bcmTotalRectifierCurrent",
+		"bcmTotalInverterCurrent",
+		"bcmMaxBatteryTemperature",
+		"bcmMaxSystemTemperature",
+	}
 
 	// Default is a pointer to a GoSNMP struct that contains sensible defaults
 	// eg port 161, community public, etc
@@ -23,28 +53,30 @@ func main() {
 	}
 	defer g.Default.Conn.Close()
 
-	//oids := []string{"1.3.6.1.2.1.1.4.0", "1.3.6.1.2.1.1.7.0"}
-	oids := []string{".1.3.6.1.4.1.26896.1.1.106.0", ".1.3.6.1.4.1.26896.1.1.112.0"}
+	oids := paramBCMmib
+	//oids := []string{"1.3.6.1.2.1.1.4", "1.3.6.1.2.1.1.7"}
+	//oids := []string{".1.3.6.1.4.1.26896.1.1.106.0", ".1.3.6.1.4.1.26896.1.1.112.0"}
 	//oids := []string{".1.3.6.1.4.1.26896.1.1.111"}
 
-	result, err2 := g.Default.Get(oids) // Get() accepts up to g.MAX_OIDS
+	result, err2 := g.Default.GetNext(oids) // Get() accepts up to g.MAX_OIDS
 	if err2 != nil {
 		log.Fatalf("Get() err: %v", err2)
 	}
 
 	for i, variable := range result.Variables {
-		fmt.Printf("%d: oid: %s ", i, variable.Name)
+		//fmt.Printf("%d: oid: %s ", i, variable.Name)
+		if i == 0 {
+			fmt.Printf("{")
+		}
+		fmt.Printf("\"%s\":", paramBCM[i])
 
-		// the Value of each variable returned by Get() implements
-		// interface{}. You could do a type switch...
-		switch variable.Type {
-		case g.OctetString:
-			fmt.Printf("string: %s\n", string(variable.Value.([]byte)))
-		default:
-			// ... or often you're just interested in numeric values.
-			// ToBigInt() will return the Value as a BigInt, for plugging
-			// into your calculations.
-			fmt.Printf("number: %d\n", g.ToBigInt(variable.Value))
+		a, _ := new(big.Float).SetInt(g.ToBigInt(variable.Value)).Float64()
+		c := a / float64(1000)
+		if i == len(result.Variables)-1 {
+			fmt.Printf("%.2f,", c)
+		} else {
+			fmt.Printf("%.2f,", c) //!!!!!!!!!!! походу это лишнее
 		}
 	}
+	fmt.Printf("\"version\": \"%s\"}", version)
 }
